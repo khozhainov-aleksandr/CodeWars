65c01ca6ff86f51100f50a4b3ea4270dd749d64d
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-17 14:37:36 +0200

    Descending Order

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+// Descending Order
+
+function descendingOrder(number){
+    return Number( String(number).split('').sort((a,b) => (b-a)).join('') );
+}
+
+console.log( descendingOrder(145263) ); // 654321
