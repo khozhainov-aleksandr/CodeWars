0ff4f7c42b8cc1e7ec9bbfa87cbb1982cf64f1a7
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-19 01:03:51 +0200

    Sum of two lowest positive integers

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,21 +1,11 @@
 'use strict';
 
-// Beginner Series #3 Sum of Numbers
+// Sum of two lowest positive integers
 
-function getSum(a, b) {
-    const x = (a < b) ? a : b;
-    const z = (a > b) ? a : b;
-    let sum = [];
-
-    for(let i = x; i <= z; i++) {
-        sum.push(i);
-    }
-
-    return (a === b) ? a : sum.reduce((sum, el) => (sum + el));
+function sumTwoSmallestNumbers(numbers) {  
+    const [firstNumber, secondNumber] = numbers.sort((a,b) => (a-b));
+    return firstNumber + secondNumber;
 }
 
-console.log( getSum(5, -3) ); // 9
-console.log( getSum(0, -1) ); // -1
-console.log( getSum(0, 1) ); // 1
-console.log( getSum(1, 1) ); // 1
-console.log( getSum(-1, 2) ); // 2
+console.log( sumTwoSmallestNumbers([5, 8, 12, 19, 22]) ); // 13
+console.log( sumTwoSmallestNumbers([23, 71, 33, 82, 1]) ); // 24
