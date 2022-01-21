b4a2ae6feaa317d9178e9e354d2a4b81684256d4
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-22 00:05:39 +0200

    No Loops 1 - Small enough?

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// Small enough? - Beginner
+// No Loops 1 - Small enough?
 
-function smallEnough(arr, limit) {
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] >= limit) { return false; }
-    }
-    
-    return true;
+function smallEnough(a, limit) {
+    return Math.max(...a) <= limit;
 }
 
-console.log( smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100) ); // false
+console.log( smallEnough([101, 45, 75, 105, 99, 107], 107) ); // true
 console.log( smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120) ); // true
+console.log( smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100) ); // false
