c2b10749c8a0a79b9a04cc01b358730e8aa78638
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-22 00:31:11 +0200

    No Loops 2 - You only need one

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-// No Loops 1 - Small enough?
+// No Loops 2 - You only need one
 
-function smallEnough(a, limit) {
-    return Math.max(...a) <= limit;
+function check(a, x) {
+    return a.includes(x);
 }
 
-console.log( smallEnough([101, 45, 75, 105, 99, 107], 107) ); // true
-console.log( smallEnough([80, 117, 115, 104, 45, 85, 112, 115], 120) ); // true
-console.log( smallEnough([78, 117, 110, 99, 104, 117, 107, 115], 100) ); // false
+console.log( check([66, 101], 66) ); // true
+console.log( check([80, 117, 115, 104, 45, 85, 112, 115], 45) ); // true
+console.log( check(['what', 'a', 'great', 'kata'], 'kat') ); // false
