de2eda1d7269926be6d596540f69469352dc043f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-23 08:15:04 +0200

    Is every value in the array an array?

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,14 +1,11 @@
 'use strict';
 
-// No Loops 3 - Copy Within
+// Is every value in the array an array?
 
-function copy(array, start, stop, place) {
-    const newCopyArray = array.slice(start, stop);
-    array.splice(place, newCopyArray.length, ...newCopyArray);
+const arrCheck = value => value.every(el => Array.isArray(el));
 
-    return array;
-}
+console.log( arrCheck([]) ); // true
+console.log( arrCheck([[1],[2],[3]]) ); // true
 
-console.log( copy([1, 2, 3, 4, 5], 0, 2, -2) ); // [1, 2, 3, 1, 2]
-console.log( copy([1, 2, 3, 4, 5], 3, 4, 0) ); // [4, 2, 3, 4, 5]
-console.log( copy(["Banana", "Orange", "Apple", "Mango"], 0, 2, 2) ); // ["Banana", "Orange", "Banana", "Orange"]
+console.log( arrCheck([[],{}]) ); // false
+console.log( arrCheck(['A', 'R', 'R', 'A', 'Y']) ); // false
