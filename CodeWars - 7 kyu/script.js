66b92c61c8b1f2d79430727179768ecf7a4e096a
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-25 07:14:03 +0200

    Beginner Series #2 Clock

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,11 +1,10 @@
 'use strict';
 
-// L1: Set Alarm
+// Beginner Series #2 Clock
 
-function setAlarm(employed, vacation){
-    return (employed === true && vacation === false);
+function past(h, m, s){
+    return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
 }
 
-console.log(setAlarm(false,true)); // false
-console.log(setAlarm(true, false)); // true
-console.log(setAlarm(true, true)); // false
+console.log(past(0,1,1)); // 61000
+console.log(past(1,1,1)); // 3661000
