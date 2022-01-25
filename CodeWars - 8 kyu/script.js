a5084dc83b71960d8b841038038a8c07c02b4849
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-25 23:53:19 +0200

    Calculate average

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,24 +1,11 @@
 'use strict';
 
-// Count of positives / sum of negatives
+// Calculate average
 
-function countPositivesSumNegatives(input) {
-    let x = 0;
-    let z = 0;
+function findAverage(array) {
+    return (array.toString() === '') ? 0 : array.reduce((sum, el) => (sum + el), 0) / array.length;
+  }
 
-    if (input === null || input.length < 1) {return [];}
-
-    for (let key in input) {
-        if (input[key] > 0) {
-            x += 1;
-        } else {
-            z += input[key];
-        }
-    }
-    
-    return [x, z];
-}
-
-console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])); // [10, -65]
-console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])); // [8, -50]
-console.log(countPositivesSumNegatives([])); // []
+console.log(findAverage([1,2,3])); // 2
+console.log(findAverage([1,2,3,4])); // 2.5
+console.log(findAverage([])); // 0
