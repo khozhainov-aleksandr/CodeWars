75d85b33f415503fb4a67a1db6fad77557c0e33d
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-25 14:54:02 +0200

    Count of positives / sum of negatives

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,2 +1,24 @@
 'use strict';
 
+// Count of positives / sum of negatives
+
+function countPositivesSumNegatives(input) {
+    let x = 0;
+    let z = 0;
+
+    if (input === null || input.length < 1) {return [];}
+
+    for (let key in input) {
+        if (input[key] > 0) {
+            x += 1;
+        } else {
+            z += input[key];
+        }
+    }
+    
+    return [x, z];
+}
+
+console.log(countPositivesSumNegatives([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15])); // [10, -65]
+console.log(countPositivesSumNegatives([0, 2, 3, 0, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14])); // [8, -50]
+console.log(countPositivesSumNegatives([])); // []
