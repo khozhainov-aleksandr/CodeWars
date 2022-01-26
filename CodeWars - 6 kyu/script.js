0e957cac28fe72561826dc4d08fb81dfc48bd264
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-27 01:37:12 +0200

    Stop gninnipS My sdroW!

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,18 +1,11 @@
 'use strict';
 
-// Find the odd int
+// Stop gninnipS My sdroW!
 
-function findOdd(arr) {
-    const sortArr = arr.sort((a,b) => a-b);
-    let count = 0;
-
-    for (let i = 0; i < sortArr.length; i++) {
-        for (let j = 0; j < sortArr.length; j++) {
-            if (sortArr[i] === sortArr[j]) { count++; }
-        }
-        if (count % 2 !== 0) { return sortArr[i]; }
-    }
+function spinWords(string){
+    return string.split(' ').map(el => (el.length >= 5) ? el.split('').reverse().join('') : el).join(' ');
 }
 
-console.log(findOdd([20,1,-1,2,-2,3,3,5,5,1,2,4,20,4,-1,-2,5])); // 5
-console.log(findOdd([1,1,2,-2,5,2,4,4,-1,-2,5])); // -1
+console.log(spinWords("Welcome")); // "emocleW"
+console.log(spinWords("Hey fellow warriors")); // "Hey wollef sroirraw"
+console.log(spinWords("Seriously this is the last one")); // "ylsuoireS this is the last one"
