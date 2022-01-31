341e4178cd96db11420bb2af5b8ab32a48dfc8da
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-31 21:00:20 +0200

    Get the largest number

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,31 +1,9 @@
 'use strict';
 
-// Get average age
+// Get the largest number
 
-function getAverageAge(years) {
-    let res = [];
-
-    if (years.toString() === '') { return 0; }
-
-    for (let i = 0; i < years.length; i++) {
-        let year = years[i].split('-').map(el => Number(el));
-        res.push(year[1] - year[0]);
-    }
-    
-    return Math.round( res.reduce((sum, el) => (sum + el)) / years.length );
+function getLargestNumber(numbers) {
+    return Math.max(...numbers);
 }
 
-console.log(getAverageAge([
-    '1907-1997',
-    '1761-1833',
-    '1535-1582',
-    '1918-2012',
-    '1877-1968',
-    '1696-1724',
-    '1602-1642',
-    '1692-1743',
-    '1695-1762',
-    '1570-1636',
-    '1762-1807',
-    '1668-1731',
-  ])); // 63
+console.log(getLargestNumber([7, 7, 7, 7.1])); // 7.1
