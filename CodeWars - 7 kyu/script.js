84ec90197fbb2f68b9dc3b6a3c9152194b119f11
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-01-31 20:58:29 +0200

    Get average age

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,10 +1,31 @@
 'use strict';
 
-// Beginner Series #2 Clock
+// Get average age
 
-function past(h, m, s){
-    return (h * 60 * 60 * 1000) + (m * 60 * 1000) + (s * 1000);
+function getAverageAge(years) {
+    let res = [];
+
+    if (years.toString() === '') { return 0; }
+
+    for (let i = 0; i < years.length; i++) {
+        let year = years[i].split('-').map(el => Number(el));
+        res.push(year[1] - year[0]);
+    }
+    
+    return Math.round( res.reduce((sum, el) => (sum + el)) / years.length );
 }
 
-console.log(past(0,1,1)); // 61000
-console.log(past(1,1,1)); // 3661000
+console.log(getAverageAge([
+    '1907-1997',
+    '1761-1833',
+    '1535-1582',
+    '1918-2012',
+    '1877-1968',
+    '1696-1724',
+    '1602-1642',
+    '1692-1743',
+    '1695-1762',
+    '1570-1636',
+    '1762-1807',
+    '1668-1731',
+  ])); // 63
