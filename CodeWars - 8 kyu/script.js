1958d7bb81b7762eb5fb92496e4a350783602357
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-03 23:21:07 +0200

    ASCII Total

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,10 +1,15 @@
 'use strict';
 
-// Vowel remover
+// ASCII Total
 
-function shortcut(string) {
-  return string.replace(/[aeiou]/gi, '');
+function uniTotal(string) {
+  let sum = 0;
+  for (let i = 0; i < string.length; i++) {
+    sum += string[i].charCodeAt();
+  }
+  return sum;
 }
 
-console.log(shortcut('hello')); // 'hll'
-console.log(shortcut('how are you today?')); // 'hw r y tdy?'
+console.log(uniTotal("e")); // 101
+console.log(uniTotal("aaa")); // 291
+console.log(uniTotal("Mary Had A Little Lamb")); // 1873
