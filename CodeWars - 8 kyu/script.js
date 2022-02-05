f83555eb6036363e52835b14287a0f05f051e6b7
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-05 23:11:54 +0200

    Well of Ideas - Easy Version

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,7 +1,14 @@
 'use strict';
 
-// Grasshopper - Check for factor
+// Well of Ideas - Easy Version
 
-function checkForFactor (base, factor) {
-    return (base % factor) === 0;
+function well(arr) {
+    let res = 0;
+    arr.filter(el => (el === 'good') ? res += 1 : res);
+    if (res === 0) { return 'Fail!'; }
+    return (res <= 2) ? 'Publish!' : 'I smell a series!';
 }
+
+console.log(well(['bad', 'bad', 'bad'])); // 'Fail!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad'])); // 'Publish!'
+console.log(well(['good', 'bad', 'bad', 'bad', 'bad', 'good', 'bad', 'bad', 'good'])); // 'I smell a series!'
