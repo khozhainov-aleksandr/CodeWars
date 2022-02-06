e8286436d4dd5a251dd606c7ab14b6fa895ed143
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-07 01:24:07 +0200

    Exclamation marks series #2: Remove all

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,21 +1,15 @@
 'use strict';
 
-// Rock Paper Scissors!
+// Exclamation marks series #2: Remove all exclamation marks from the end of sentence
 
-function rps(paper1, paper2) {
-    if (paper1 === paper2) { return 'Draw!'; }
-    if ((paper1 === 'rock' && paper2 == 'scissors') || (paper1 === 'scissors' && paper2 == 'paper') || (paper1 === 'paper' && paper2 == 'rock')) { return 'Player 1 won!'; }
-    if ((paper1 === 'scissors' && paper2 == 'rock') || (paper1 === 'paper' && paper2 == 'scissors') || (paper1 === 'rock' && paper2 == 'paper')) { return 'Player 2 won!'; }
-}
+function remove(string) { 
+    const reg = /\!+$/gi;
+    return string.replace(reg, '');
+  }
 
-console.log(rps('rock', 'scissors')); // Player 1 won!
-console.log(rps('scissors', 'paper')); // Player 1 won!
-console.log(rps('paper', 'rock')); // Player 1 won!
-
-console.log(rps('scissors', 'rock')); // Player 2 won!
-console.log(rps('paper', 'scissors')); // Player 2 won!
-console.log(rps('scissors', 'rock')); // Player 2 won!
-
-console.log(rps('rock', 'rock')); // Draw!
-console.log(rps('scissors', 'scissors')); // Draw!
-console.log(rps('paper', 'paper')); // Draw!
+console.log( remove("Hi!")); // === "Hi"
+console.log( remove("Hi!!!")); // === "Hi"
+console.log( remove("!Hi")); // === "!Hi"
+console.log( remove("!Hi!")); // === "!Hi"
+console.log( remove("Hi! Hi!")); // === "Hi! Hi"
+console.log( remove("Hi")); // === "Hi"
