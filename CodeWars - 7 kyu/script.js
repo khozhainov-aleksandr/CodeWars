1984bcff3867baac832183bd995dcea731eba3bb
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-08 23:06:51 +0200

    Multiply numbers

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,24 +1,10 @@
 'use strict';
 
-// Bank credit
+// Multiply numbers
 
-function getYears(amount, percent, limit) {
-    const percentByYear = amount * percent / 100;
-    let oneYear = amount;
-    let year = 0;
-  
-    if ((oneYear + percentByYear) === limit) {
-      return 1;
-    }
-  
-    do {
-      oneYear = oneYear + percentByYear;
-      year++;
-    } while ((limit - 1) > oneYear);
-  
-    return year - 1;
-  }
+function multiply(a, b = 1) {
+    return a * b;
+}
 
-console.log( getYears(1600, 10, 2000) ); //  === 2 1600 + 160 + 176 < 2000;
-console.log( getYears(500, 3, 550) ); //  === 3
-console.log( getYears(5000, 5, 6500) ); //  === 5
+console.log(multiply(5, 10)); // 50
+console.log(multiply(24)); // 24
