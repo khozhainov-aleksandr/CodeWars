a9703eae8b87ab997f4215e145773135eeb1f603
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-08 21:28:13 +0200

    Bank credit

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,9 +1,24 @@
 'use strict';
 
-// Unique names
+// Bank credit
 
-function getUniqNames(names) {
-    return names.filter( (element, index) => names.indexOf(element) === index );
-}
+function getYears(amount, percent, limit) {
+    const percentByYear = amount * percent / 100;
+    let oneYear = amount;
+    let year = 0;
+  
+    if ((oneYear + percentByYear) === limit) {
+      return 1;
+    }
+  
+    do {
+      oneYear = oneYear + percentByYear;
+      year++;
+    } while ((limit - 1) > oneYear);
+  
+    return year - 1;
+  }
 
-console.log(getUniqNames(['Eileen', 'Peter', 'Jared', 'Robert', 'Robert', 'Robert', 'David',])); // ['Eileen', 'Peter', 'Jared', 'Robert', 'David']
+console.log( getYears(1600, 10, 2000) ); //  === 2 1600 + 160 + 176 < 2000;
+console.log( getYears(500, 3, 550) ); //  === 3
+console.log( getYears(5000, 5, 6500) ); //  === 5
