d268935e34d0fb28679832353519742c132278e4
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-10 01:23:22 +0200

    Get robot schema

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,23 +1,25 @@
 'use strict';
 
-// Count boxes
+// Get robot schema
 
-function countBoxes(boxes) {
-    // if (boxes === '') {return {}; }
-
-    const res = {};
-    const arr = boxes.split('');
-    const eachElement = arr.filter( (element, index) => arr.indexOf(element) === index );
-
-    for (let i = 0; i < eachElement.length; i++) {
-        let key = arr.filter((element) => (element === eachElement[i])).length;
-        let value = eachElement[i];
-        res[value] = key;
+function getRobotSchema(robot) {
+    const newScheme = {};
+  
+    for (const key in robot) {
+      const value = key;
+  
+      newScheme[value] = typeof robot[key];
     }
-
-    return res;
+  
+    return newScheme;
 }
 
-console.log( countBoxes('aabca') );  // { a: 3, b: 1, c: 1 }
-console.log( countBoxes('aaaaca31') );  // { a: 5, c: 1, 3: 1, 1: 1 }
-console.log( countBoxes('') );  // {}
+const robot = {
+    version: 16,
+    name: 'Cleaner 3000',
+    released: true,
+    creator: { name: 'Vlad' },
+};
+
+console.log(robot);
+console.log(getRobotSchema(robot));
