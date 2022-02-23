7bb7afd6a26de9f4419ceba68636829b900cb97f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-23 10:06:44 +0200

    Calculate years

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,11 +1,22 @@
 'use strict';
 
-// Are in love
+// Calculate years
 
-function areInLove(flower1, flower2) {
-  return ((flower1 % 2) === 0) !== ((flower2 % 2) === 0);
+function calculateYears(principal, interest, tax, desired) {
+  const percentFromBank = principal * interest;
+  const clearTaxBank = principal * interest * tax;
+
+  let totalSum = principal;
+  let years = 0;
+  
+  for (totalSum; totalSum < desired; totalSum++) {
+    totalSum = totalSum  + (percentFromBank - clearTaxBank);
+    years++;
+  }
+
+  return years;
 }
 
-console.log( areInLove(0, 0) );
-console.log( areInLove(1, 4) );
-console.log( areInLove(2, 2) );
+console.log( calculateYears(1000, 0.05, 0.18, 1100) ); // 3
+console.log( calculateYears(1000, 0.05, 0.18, 1000) ); // 0
+console.log( calculateYears(1000, 0.01625, 0.18, 1200) ); // 14
