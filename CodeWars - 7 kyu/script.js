710b051dd91b52ee8c0e3ebd89b7d2246ada95e3
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-24 10:01:53 +0200

    Shorten to date

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,22 +1,10 @@
 'use strict';
 
-// Calculate years
+// Shorten to date
 
-function calculateYears(principal, interest, tax, desired) {
-  const percentFromBank = principal * interest;
-  const clearTaxBank = principal * interest * tax;
-
-  let totalSum = principal;
-  let years = 0;
-  
-  for (totalSum; totalSum < desired; totalSum++) {
-    totalSum = totalSum  + (percentFromBank - clearTaxBank);
-    years++;
-  }
-
-  return years;
+function shortenToDate(longDate) {
+  return longDate.split(',')[0];
 }
 
-console.log( calculateYears(1000, 0.05, 0.18, 1100) ); // 3
-console.log( calculateYears(1000, 0.05, 0.18, 1000) ); // 0
-console.log( calculateYears(1000, 0.01625, 0.18, 1200) ); // 14
+console.log( shortenToDate('Friday May 2, 7pm') ); // 'Friday May 2'
+console.log( shortenToDate('Monday January 10, 9am') ); // 'Monday January 10'
