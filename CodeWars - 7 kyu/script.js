dbea2657d34c437cc368b68e5ebd559bc3d4fa30
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-27 02:18:11 +0200

    Get product ID

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,14 +1,17 @@
 'use strict';
 
-// Color stones
+// Get product ID
 
-function colorStones(stones) {
-  return stones.split('').filter((element, index, arr) => element === arr[index + 1]).length;
+function getProductId(url) {
+  const prod = Number(url.split('-p-')[1].split('-')[0]);
+
+  if (!isNaN(prod)) {
+    return String(prod);
+  } else {
+    return url.split('-p-')[2].split('-')[0];
+  }
 }
 
-console.log( colorStones('RRGB') ); //  === 1 // 'R' нужно убрать; в результате останется 'RGB'
-console.log( colorStones('RRGGB') ); //  === 2 // 'R' и 'G' нужно убрать; в результате останется 'RGB'
-console.log( colorStones('RRRRGB') ); //  === 3 // 'RRR' нужно убрать; в результате останется 'RGB'
-console.log( colorStones('RGBRGBRGGB') ); //  === 1 // 'G' нужно убрать в результате останется 'RGBRGBRGB'
-console.log( colorStones('RGGRGBBRGRR') ); //  === 3 // 'G', 'B' и 'R' нужно убрать; в результате останется 'RGRGBRGR'
-console.log( colorStones('RRRRGGGGBBBB') ); //  === 9 // 'RRR', 'GGG' и 'BBB' нужно убрать; в результате останется 'RGB'
+console.log( getProductId('exampleshop.com/fancy-coffee-cup-p-90764-12052019.html') ); // productID is 90764
+console.log( getProductId('exampleshop.com/dry-water-just-add-water-to-get-water-p-147-24122017.html') ); // productID is 147
+console.log( getProductId('exampleshop.com/public-toilet-proximity-radar-p-942312798-01012020.html') ); // productID is 942312798
