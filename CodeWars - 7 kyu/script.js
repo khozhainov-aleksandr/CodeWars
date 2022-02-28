b2034f4a59c050cb0fe24dd033e0aa0d00a4b584
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-02-28 11:05:32 +0200

    Square digits

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,17 +1,9 @@
 'use strict';
 
-// Get product ID
+// Square digits
 
-function getProductId(url) {
-  const prod = Number(url.split('-p-')[1].split('-')[0]);
-
-  if (!isNaN(prod)) {
-    return String(prod);
-  } else {
-    return url.split('-p-')[2].split('-')[0];
-  }
+function squareDigits(num) {
+  return Number(String(num).split('').map(el => Number(el) * Number(el)).join(''));
 }
 
-console.log( getProductId('exampleshop.com/fancy-coffee-cup-p-90764-12052019.html') ); // productID is 90764
-console.log( getProductId('exampleshop.com/dry-water-just-add-water-to-get-water-p-147-24122017.html') ); // productID is 147
-console.log( getProductId('exampleshop.com/public-toilet-proximity-radar-p-942312798-01012020.html') ); // productID is 942312798
+console.log( squareDigits(58)  ); // === 2564
