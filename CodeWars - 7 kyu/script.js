37c082180d0174789b2d88438c79f690d766eca4
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-03 23:07:18 +0200

    Fix string

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,18 +1,26 @@
 'use strict';
 
-// Counting duplicates
+// Fix string
 
-function countDuplicates(str) {
-  const arrStr = str.toLowerCase().split('');
-  const res = {};
+function fixString(str) {
+  let upper = 0;
+  let lower = 0;
 
-  arrStr.forEach(element => {
-    res[element] = (res[element] || 0) + 1;
+  str.split('').forEach(element => {
+    if (element.toUpperCase() === element && element !== ' ') {
+      upper++;
+    } else if (element !== ' ') {
+      lower++;
+    }
   });
 
-  return Object.values(res).filter(el => el > 1).length;
+  if (lower >= upper) {
+    return str.toLowerCase();
+  } else {
+    return str.toUpperCase();
+  }
 }
 
-console.log(countDuplicates('abcde')); // 0 // ни один символ не повторяется более одного раза
-console.log(countDuplicates('aabbcde')); // 2 // 'a' и 'b' встречаются более одного раза
-console.log(countDuplicates('aA11')); // 2 // 'a' и '1' встречаются более одного раза
+console.log( fixString('Mate Academy') ); // 'mate academy'
+console.log( fixString('Mate ACADEmy') ); // 'MATE ACADEMY'
+console.log( fixString('maTE') ); // 'mate'
