a705018dc4ed5163dbfa811378bf7e8cd0d1eb1a
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-04 18:45:30 +0200

    Count matching socks

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,22 +1,22 @@
 'use strict';
 
-// Row weights
+// Count matching socks
 
-function getRowWeights(array) {
-  let one = 0;
-  let two = 0;
+function countMatchingSocks(colors) {
+  const obj = {};
 
-  array.forEach((element, index) => {
-    if (index % 2 === 0) {
-      one += element;
-    } else {
-      two += element;
-    }
+  colors.forEach(element => {
+    obj[element] = (obj[element] || 0) + 1;
   });
 
-  return [one, two];
+  let sum = Object.values(obj)
+    .filter(el => el > 1)
+    .reduce((sum, el) => sum + el, 0);
+
+  return Math.trunc(sum / 2);
 }
 
-console.log( getRowWeights([10]) ); // [10, 0]
-console.log( getRowWeights([10, 85, 90]) ); // [100, 85]
-console.log( getRowWeights([8, 5, 9, 3]) ); // [17, 8]
+console.log( countMatchingSocks([10, 10]) ); // - одна пара носков цвета 10.
+console.log( countMatchingSocks([2, 2, 2, 2, 2]) ); // - две пары носков цвета 2.
+console.log( countMatchingSocks([10, 20, 30, 40, 50, 60]) ); // - все носки разного цвета, ни одной пары.
+console.log( countMatchingSocks([10, 20, 30, 10, 20, 60]) ); // - 2 пары, одна цвета 10, вторая - 20.
