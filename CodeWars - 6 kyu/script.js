dcdc893dfe0b40f6c8ef0a5c0534f6c6d79106df
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-04 21:37:05 +0200

    Find smaller digits

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,9 +1,25 @@
 'use strict';
 
-// Create Phone Number
+// Find smaller digits
 
-function createPhoneNumber(numbers){
-    return numbers.reduce((sum, el) => sum.replace('X', el), '(XXX) XXX-XXXX');
+function findSmallerDigits(arr) {
+  const resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let sum = 0;
+
+    for (let j = i; j < arr.length; j++) {
+      if (arr[j] < arr[i]) {
+        sum++;
+      }
+    }
+    resultArr.push(sum);
+  }
+
+  return resultArr;
 }
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])); // "(123) 456-7890"
+console.log( findSmallerDigits([5, 4, 3, 2, 1]) ); // [4, 3, 2, 1, 0]
+console.log( findSmallerDigits([1, 2, 0]) ); // [1, 1, 0]
+console.log( findSmallerDigits([1, 1, -1, 0, 0]) ); // [3, 3, 0, 0, 0]
+console.log( findSmallerDigits([5, 4, 7, 9, 2, 4, 4, 5, 6]) ); // [4, 1, 5, 5, 0, 0, 0, 0, 0]
