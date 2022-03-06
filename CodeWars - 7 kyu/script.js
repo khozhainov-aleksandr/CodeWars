970acc9aa7504a5d5565e57cf5352215a095a612
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-06 23:34:54 +0200

    Add numbers from object

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,38 +1,20 @@
 'use strict';
 
-// Who is the killer
+// Add numbers from object
 
-const person = {
-  'Peter': ['Lucy', 'Kyle'],
-  'James': ['Jacob', 'Bill', 'Lucas'],
-  'Johnny': ['David', 'Kyle', 'Lucas'],
+const object = {
+  foo: 'js',
+  bar: 'fe',
+  boo: 3,
+  spam: 10,
+  egg: 11,
 };
 
-const dead = ['Lucas', 'Bill'];
-
-// function getKiller(suspects, dead) {
-//   return Object.keys(suspects).find(x => dead.every(y => suspects[x].includes(y)));
-// }
-
-function getKiller(suspects, dead) {
-  const allKillers = Object.keys(suspects);
-  const allDeads = Object.values(suspects);
-
-  let arrRes = [];
-
-  for (let i = 0; i < allDeads.length; i++) {
-
-    let eachPerson = [allKillers[i]];
-    for (let j = 0; j < allDeads[i].length; j++) {
-      if ( dead.includes(allDeads[i][j]) ) {
-        eachPerson.push(allDeads[i][j]);
-      }
-    }
-
-    arrRes.push(eachPerson);
-  }
-
-  return arrRes.sort((a,b) => b.length - a.length)[0][0];
+function addNumbers(object) {
+  return Object
+    .values(object)
+    .filter(el => typeof(el) === 'number')
+    .reduce((sum, el) => sum + el);
 }
 
-console.log( getKiller(person, dead) ); // James
+console.log( addNumbers(object) ); // return 24
