211603d54f9ed761f12278ee7a5103af5d62bcdb
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-11 10:17:26 +0200

    Calculate averages

--- CodeWars - 7 kyu/script.js
+++ CodeWars - 7 kyu/script.js
@@ -1,19 +1,17 @@
 'use strict';
 
-// Vowels count
+// Calculate averages
 
-function countVowels(str) {
-  const x = ['a', 'e', 'i', 'o', 'u'];
-  let res = 0;
+const calculator = {
+  average: function(...res) {
+    if (res.toString() === '') {
+      return 0;
+    }
 
-  x.forEach(element => {
-    res += str.split('').filter(el => element === el).length;
-  });
+    return res.reduce((sum, el) => sum + el) / res.length;
+  },
+};
 
-  return res;
-}
-
-console.log( countVowels('hello world') ); // === 3
-console.log( countVowels('ooooo') ); // === 5
-console.log( countVowels('qwrtp') ); // === 0
-console.log( countVowels('gym') ); // === 0 // 'y' не считается гласной
+console.log( calculator.average() ); // === 0
+console.log( calculator.average(1, 2, 3) ); // === 2
+console.log( calculator.average(-3, -1, 0, 1) ); // === -0.75
