64f0dfd40940e4e412a28fe38ec68870a022bac8
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-31 13:46:05 +0300

    Inverse robot

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,26 +1,26 @@
 'use strict';
 
-// Upgrade robot
+// Inverse robot
 
-function upgradeRobot(robot, parts) {
-  return Object.assign(robot, ...parts);
+function inverseRobot(robot) {
+  const arr = Object.entries(robot);
+  const duplicateValues = {};
+
+  arr.forEach(element => {
+    duplicateValues[element[1]] = (duplicateValues[element[1]] || 0) + 1;
+  });
+
+  const amountDuplicateValues = Object.values(duplicateValues);
+  if (amountDuplicateValues.some(el => el > 1)) {
+    return null;
+  }
+
+  const reverseKeyAndValue = arr.map(element => [element[1], element[0]]).reverse();
+  return Object.fromEntries(reverseKeyAndValue);
 }
 
-const kobi = {
-  chipVer: 9,
-  serialNo: 413,
-  wheels: 2,
-}
+const kolli = { Kolli: 'name', 123: 'chipVer', 3: 'wheels' };
+const robert = { Robert: 'name', 123: 'chipVer', Alex: 'surname', 113: 'chipVer' };
 
-const parts = [
-  { wheels: 6 },
-  { chipVer: 16 },
-  { displays: 2 },
-];
-
-upgradeRobot(kobi, parts);
-
-console.log( kobi.serialNo ); // === 413
-console.log( kobi.wheels ); // === 6
-console.log( kobi.chipVer ); // === 16
-console.log( kobi.displays ); // === 2
+console.log( inverseRobot(robert) ); // === null
+console.log( inverseRobot(kolli) ); // === { name: 'Kolli', chipVer: '123', wheels: '3' }
