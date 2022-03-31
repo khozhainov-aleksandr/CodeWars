813e082b694a99e8461f0cbbc3f72f3fd4df2381
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-03-31 12:34:17 +0300

    Upgrade robot

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,10 +1,26 @@
 'use strict';
 
-// Register robot
+// Upgrade robot
 
-function registerRobot(robot, warehouse) {
-  warehouse.aiStaff.push(robot.id);
-  robot.currentWorkPlace = warehouse;
+function upgradeRobot(robot, parts) {
+  return Object.assign(robot, ...parts);
 }
 
-registerRobot();
+const kobi = {
+  chipVer: 9,
+  serialNo: 413,
+  wheels: 2,
+}
+
+const parts = [
+  { wheels: 6 },
+  { chipVer: 16 },
+  { displays: 2 },
+];
+
+upgradeRobot(kobi, parts);
+
+console.log( kobi.serialNo ); // === 413
+console.log( kobi.wheels ); // === 6
+console.log( kobi.chipVer ); // === 16
+console.log( kobi.displays ); // === 2
