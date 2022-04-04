d85d7689b828d7a13e2a552676b3b4b3046d9e63
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-04 18:28:57 +0300

    Multiply integers

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,20 +1,32 @@
 'use strict';
 
-// Contain letters
+// Multiply integers
 
-function containLetters(str1, str2) {
-  const str1Arr = str1.toLowerCase().split('');
-  const str2Arr = str2.toLowerCase().split('');
-  const res = str1Arr.filter(el => str2Arr.includes(el)).join('').length;
-
-  if (res === str2.length) {
-    return true;
-  } else {
-    return false;
+function multiplyNums(nums) {
+  if (nums.toString() === '') {
+    return 1;
   }
+
+  const obj = {};
+  const x = [];
+  const y = [];
+
+  nums.forEach(element => {
+    obj[element] = (obj[element] || 0) + 1;
+  });
+
+  for (const key in obj) {
+    if (obj[key] === 2) {
+      y.push(Number(key));
+    }
+
+    if (obj[key] === 1) {
+      x.push(Number(key));
+    }
+  }
+
+  return [...x,...x,...y].reduce((sum, el) => sum * el);
 }
 
-console.log( containLetters('rkqodlw', 'world') ); // === true
-console.log( containLetters('rkqodlw', 'woorld') ); // === false
-console.log( containLetters('scriptjava', 'javascript') ); // === true
-console.log( containLetters('katas', 'steak') ); // === false
+console.log( multiplyNums([1, 1, 1, 2, 2, 3]) ); // === 18 // 3 x 3 x 2 = 18
+console.log( multiplyNums([6, 5, 4, 10, 6, 5, 4, 10, 6, 5, 4, 20]) ); // === 4000 // 20 x 20 x 10 = 4000
