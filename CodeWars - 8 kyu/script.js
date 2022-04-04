31ef5d08f9e354755c2abd9fc2eb1cd25d58af36
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-04 22:21:12 +0300

    Shop cart

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,9 +1,34 @@
 'use strict';
 
-// Format data
+// Shop cart
 
-function formatDate(date) {
-  return date.split('-').reverse().join('.');
+function makeOrderList(str) {
+  const productNameAndQuantity = str.split(',').map(el => el.trim());
+  const res = [];
+
+  if (str === '') {
+    return {};
+  }
+
+  productNameAndQuantity.forEach(element => {
+    const elementToArr = element.split(' ');
+    const quantityGoods = Number(elementToArr[0]);
+    const productName = elementToArr.splice(1, (elementToArr.length)).join('_');
+
+    res.push([productName, quantityGoods]);
+  });
+
+  return Object.fromEntries(res);
 }
 
-console.log( formatDate('2020-02-18') ); // '18.02.2020'
+const order = '1 coca cola, 5 chicken nuggets, 20 egg';
+const list = makeOrderList(order);
+console.log( list );
+
+/*
+list === {
+  coca_cola: 1,
+  chicken_nuggets: 5,
+  egg: 20,
+}
+*/
