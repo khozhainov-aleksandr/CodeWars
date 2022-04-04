2414cdcf189c26aa5c3abc556cf547958d6cdc7b
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-04 20:14:59 +0300

    Count all pairs

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,32 +1,27 @@
 'use strict';
 
-// Multiply integers
+// Count all pairs
 
-function multiplyNums(nums) {
-  if (nums.toString() === '') {
-    return 1;
-  }
-
+function countPairs(nums) {
   const obj = {};
-  const x = [];
-  const y = [];
+  let sum = 0;
+
+  if (nums.length === 1 || nums.length === 0) {
+    return 0;
+  }
 
   nums.forEach(element => {
     obj[element] = (obj[element] || 0) + 1;
   });
-
+  
   for (const key in obj) {
-    if (obj[key] === 2) {
-      y.push(Number(key));
-    }
-
-    if (obj[key] === 1) {
-      x.push(Number(key));
+    if (obj[key] % 2 === 0) {
+      sum += obj[key];
     }
   }
 
-  return [...x,...x,...y].reduce((sum, el) => sum * el);
+  return sum / 2;
 }
-
-console.log( multiplyNums([1, 1, 1, 2, 2, 3]) ); // === 18 // 3 x 3 x 2 = 18
-console.log( multiplyNums([6, 5, 4, 10, 6, 5, 4, 10, 6, 5, 4, 20]) ); // === 4000 // 20 x 20 x 10 = 4000
+console.log( countPairs([1]) ); // === 0
+console.log( countPairs([0, 0, 0, 0]) ); // === 2
+console.log( countPairs([1, 2, 2, 20, 6, 20, 2, 6, 2]) ); // === 4
