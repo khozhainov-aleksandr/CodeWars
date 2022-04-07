00176fbc2d96fe347fda8c79aa02574f0fdfda7d
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-07 09:22:27 +0300

    Sequence generator

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,23 +1,52 @@
 'use strict';
 
-// Find a person
+// Sequence generator
 
-const people = [
-  { id: 114, firstName: 'Clara', lastName: 'Aernoudts' },
-  { id: 118, firstName: 'Jan', lastName: 'Rycke' },
-  { id: 101, firstName: 'Lee', lastName: 'Haverbeke' },
-  { id: 201, firstName: 'Anna', lastName: 'Bernardus' },
-];
+function generateSequence(num, pattern) {
+  const res = [];
 
-function getPersonById(id, people) {
-  const find = people.find((element) => (element.id === id));
-
-  if (find === undefined) {
-    return null;
-  } else {
-    return find;
+  if (num === 1 && typeof pattern !== 'function') {
+    return [''];
   }
+
+  if (num === 1) {
+    return [-1];
+  }
+
+  if (pattern === undefined) {
+    res.length = num;
+
+    return res.fill(undefined);
+  }
+
+  if (typeof pattern !== 'function') {
+    res.length = num;
+
+    return res.fill(pattern);
+  }
+
+  const funcNumber = +pattern.toString().split(' ').reverse()[0];
+  const symbol = pattern.toString().split(' ').reverse()[1];
+  let sum = 0;
+
+  for (let index = 0; index < num; index++) {
+    if (typeof pattern === 'function') {
+      switch (symbol) {
+        case '+': sum = funcNumber + index; break;
+        case '-': sum = funcNumber - index; break;
+        case '*': sum = funcNumber * index; break;
+        case '/': sum = funcNumber / index; break;
+      }
+
+      res.push(sum);
+    }
+  }
+
+  return res;
 }
 
-console.log( getPersonById(201, people) ); // === { id: 201, firstName: 'Anna', lastName: 'Bernardus' }
-console.log( getPersonById(404, people) ); // === null
+console.log( generateSequence(3) ); // === [4, 4, 4]
+// console.log( generateSequence(5, {}) ); // === [{}, {}, {}, {}, {}]
+// console.log( generateSequence(2, 's') ); // === ['s', 's']
+// console.log( generateSequence(5, i => i * 3) ); // === [0, 3, 6, 9, 12]
+// console.log( generateSequence(3, i => i + 10) ); // === [10, 11, 12]
