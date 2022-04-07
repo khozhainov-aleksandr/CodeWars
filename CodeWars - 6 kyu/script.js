6613827b526e02ac60d093855e387998e1256d25
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-07 22:57:36 +0300

    Sort names

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,14 +1,33 @@
 'use strict';
 
-// If function
+// Sort names
 
-function _if(bool, func1, func2) {
-  if (bool) {
-    return func1();
-  } else {
-    return func2();
-  }
+const people = [
+  { id: 118, firstName: 'Jan', lastName: 'Rycke' },
+  { id: 101, firstName: 'Lee', lastName: 'Haverbeke' },
+  { id: 114, firstName: 'Clara', lastName: 'Aernoudts' },
+  { id: 201, firstName: 'Anna', lastName: 'Bernardus' },
+  { id: 204, firstName: 'Lieven', lastName: 'Causmaecker' },
+  { id: 205, firstName: 'Maria', lastName: 'Sturm' },
+];
+
+
+function getEmployeesList(people) {
+  const res = [];
+
+  people.forEach(element => {
+    res.push(`${element.firstName} ${element.lastName}`);
+  });
+
+  return res.sort();
 }
 
-_if(true, () => 'True', () => 'False') // 'True'
-_if(false, () => 'True', () => 'False') // 'False'
+
+console.log( getEmployeesList(people) ); // === [
+//   'Anna Bernardus',
+//   'Clara Aernoudts',
+//   'Jan Rycke',
+//   'Lee Haverbeke',
+//   'Lieven Causmaecker',
+//   'Maria Sturm'
+// ];
