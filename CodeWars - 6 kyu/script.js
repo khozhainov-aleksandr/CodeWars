2a4a861a6fb4a44e5b271ffd041c0ed0356227bc
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-07 09:29:31 +0300

    If function

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,52 +1,14 @@
 'use strict';
 
-// Sequence generator
+// If function
 
-function generateSequence(num, pattern) {
-  const res = [];
-
-  if (num === 1 && typeof pattern !== 'function') {
-    return [''];
+function _if(bool, func1, func2) {
+  if (bool) {
+    return func1();
+  } else {
+    return func2();
   }
-
-  if (num === 1) {
-    return [-1];
-  }
-
-  if (pattern === undefined) {
-    res.length = num;
-
-    return res.fill(undefined);
-  }
-
-  if (typeof pattern !== 'function') {
-    res.length = num;
-
-    return res.fill(pattern);
-  }
-
-  const funcNumber = +pattern.toString().split(' ').reverse()[0];
-  const symbol = pattern.toString().split(' ').reverse()[1];
-  let sum = 0;
-
-  for (let index = 0; index < num; index++) {
-    if (typeof pattern === 'function') {
-      switch (symbol) {
-        case '+': sum = funcNumber + index; break;
-        case '-': sum = funcNumber - index; break;
-        case '*': sum = funcNumber * index; break;
-        case '/': sum = funcNumber / index; break;
-      }
-
-      res.push(sum);
-    }
-  }
-
-  return res;
 }
 
-console.log( generateSequence(3) ); // === [4, 4, 4]
-// console.log( generateSequence(5, {}) ); // === [{}, {}, {}, {}, {}]
-// console.log( generateSequence(2, 's') ); // === ['s', 's']
-// console.log( generateSequence(5, i => i * 3) ); // === [0, 3, 6, 9, 12]
-// console.log( generateSequence(3, i => i + 10) ); // === [10, 11, 12]
+_if(true, () => 'True', () => 'False') // 'True'
+_if(false, () => 'True', () => 'False') // 'False'
