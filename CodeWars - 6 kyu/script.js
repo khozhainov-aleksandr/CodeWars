10e1d94e61539fcc8b9f3a6f4d7faaeb05fce835
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-10 13:47:07 +0300

    Some method

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,52 +1,27 @@
 'use strict';
 
-// Find index
-
-// const people = [
-//   { id: 118, firstName: 'Jan', lastName: 'Rycke' },
-//   { id: 101, firstName: 'Lee', lastName: 'Haverbeke' },
-//   { id: 114, firstName: 'Clara', lastName: 'Aernoudts' },
-//   { id: 201, firstName: 'Anna', lastName: 'Bernardus' },
-// ];
-
-// const getPersonIndex = (people, nameEndsWith) => {
-  // const res = [];
-
-  // people.forEach(el => {
-  //   const enterIndex = el.firstName.split('').reverse().findIndex(indexElement => indexElement === nameEndsWith);
-  //   res.push(enterIndex);
-  // });
-  // console.log( res );
-
-  // for (let i = 0; i < res.length; i++) {    
-  //   if (res.some(el => el === i)) {
-  //     console.log( i );
-  //   }
-  // }
-// };
-
-// console.log( getPersonIndex(people, 'a') ); // === 2
-// console.log( getPersonIndex(people, 'k') ); // === null
-
-// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
-
 // Some method
 
 const numbers = [1, 2, 3, 4, 10, 8];
 
-numbers.some = function(callBack) {
+numbers.every = function(callBack) {
+  let count = 0;
+
   for (let i = 0; i < this.length; i++) {
     const element = callBack(this[i], i, this);
 
     if (element) {
-      return true;
+      count++;
     }
   }
 
-  return false;
+  if (count === this.length) {
+    return true;
+  } else {
+    return false;
+  }
 };
 
-const hasNegativeNumbers = numbers.some(el => el < 0);
+const allNumbersArePositive = numbers.every(el => el > 0);
 
-console.log( hasNegativeNumbers ); // === false;
-console.log( numbers ); // === [1, 2, 3, 4, 10, 8]
+console.log( allNumbersArePositive ); // === true;
