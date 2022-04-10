b200a9bdf30b4b6c44937f77082d01f52e2b2249
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-10 13:37:14 +0300

    Some method

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,20 +1,52 @@
 'use strict';
 
-// Find method
+// Find index
+
+// const people = [
+//   { id: 118, firstName: 'Jan', lastName: 'Rycke' },
+//   { id: 101, firstName: 'Lee', lastName: 'Haverbeke' },
+//   { id: 114, firstName: 'Clara', lastName: 'Aernoudts' },
+//   { id: 201, firstName: 'Anna', lastName: 'Bernardus' },
+// ];
+
+// const getPersonIndex = (people, nameEndsWith) => {
+  // const res = [];
+
+  // people.forEach(el => {
+  //   const enterIndex = el.firstName.split('').reverse().findIndex(indexElement => indexElement === nameEndsWith);
+  //   res.push(enterIndex);
+  // });
+  // console.log( res );
+
+  // for (let i = 0; i < res.length; i++) {    
+  //   if (res.some(el => el === i)) {
+  //     console.log( i );
+  //   }
+  // }
+// };
+
+// console.log( getPersonIndex(people, 'a') ); // === 2
+// console.log( getPersonIndex(people, 'k') ); // === null
+
+// ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- //
+
+// Some method
 
 const numbers = [1, 2, 3, 4, 10, 8];
 
-numbers.myFind = function(callBack) {
+numbers.some = function(callBack) {
   for (let i = 0; i < this.length; i++) {
-    const x = callBack(this[i], i, this);
+    const element = callBack(this[i], i, this);
 
-    if (x) {
-      return this[i];
+    if (element) {
+      return true;
     }
   }
+
+  return false;
 };
 
-const foundNumber = numbers.myFind(el => el > 4);
+const hasNegativeNumbers = numbers.some(el => el < 0);
 
-console.log( foundNumber ); // === 10;
+console.log( hasNegativeNumbers ); // === false;
 console.log( numbers ); // === [1, 2, 3, 4, 10, 8]
