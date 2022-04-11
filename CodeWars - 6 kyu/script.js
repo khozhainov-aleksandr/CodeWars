07b51ac364a5d1479e9e6708419fd35bcd21e058
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-11 08:36:05 +0300

    Reverse words

--- CodeWars - 6 kyu/script.js
+++ CodeWars - 6 kyu/script.js
@@ -1,10 +1,18 @@
 'use strict';
 
-// Remove string
+// Reverse words
 
-function removeStrings(arr) {
-  return arr.filter(el => typeof el !== 'string');
+function reverseWords(str) {
+  const array = str.split(' ');
+  const result = [];
+
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i].split('').reverse().join('');
+    result.push(element);
+  }
+
+  return result.join(' ');
 }
 
-console.log( removeStrings([1, 2, 'a', 'b']) ); // === [1, 2]
-console.log( removeStrings(['', 0, 10, '100']) ); // === [0, 10]
+console.log( reverseWords('This is an example!') ); // === 'sihT si na !elpmaxe'
+console.log( reverseWords('double  spaces') ); // === 'elbuod  secaps'
