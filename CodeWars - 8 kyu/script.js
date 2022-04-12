b0a157d7ca9e42f12d6fd8defe95ef0bc782d59f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-12 10:35:37 +0300

    Get people lived at least 65 years

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,6 +1,33 @@
 'use strict';
 
-// Get women died before 1800
+// Get father
+
+// const people = [
+//   { name: 'Carolus Haverbeke', sex: 'm', born: 1832, died: 1905, father: 'Carel Haverbeke', mother: 'Maria van Brussel' },
+//   { name: 'Emma de Milliano', sex: 'f', born: 1876, died: 1956, father: 'Petrus de Milliano', mother: 'Sophia van Damme' },
+//   { name: 'Maria de Rycke', sex: 'f', born: 1683, died: 1724, father: 'Frederik de Rycke', mother: 'Laurentia van Vlaenderen' },
+//   { name: 'Carel Haverbeke', sex: 'm', born: 1796, died: 1837, father: 'Pieter Antone Haverbeke', mother: 'Livina Sierens' },
+// ];
+
+// function getFather(people, person) {
+//   if (people.toString() === '') {
+//     return null;
+//   }
+
+//   if (people.some(el => person.father === el.name && el.sex === 'm')) {
+//     return people[people.length - 1];
+//   } else {
+//     return null;
+//   }
+// }
+
+// const person = people[0];
+
+// console.log("🚀 ~ file:", getFather(people, person)) // === people[3];
+
+// ------------------------------------------------------------------------------------------------------------------------------ //
+
+// Get people lived at least 65 years
 
 const people = [
   { name: 'Carolus Haverbeke', sex: 'm', born: 1832, died: 1905, father: 'Carel Haverbeke', mother: 'Maria van Brussel' },
@@ -9,8 +36,9 @@ const people = [
   { name: 'Carel Haverbeke', sex: 'm', born: 1796, died: 1837, father: 'Pieter Antone Haverbeke', mother: 'Livina Sierens' },
 ];
 
-function getWomenDiedBefore1800(people) {
-  return people.filter(el => el.sex === 'f' && el.died < 1800);
+function getPeopleLivedMin65Years(people) {
+  return people.filter(el => (el.died - el.born) < 65);
 }
 
-console.log("🚀 ~", getWomenDiedBefore1800(people)) // === [people[2]];
+console.log("🚀 ~", getPeopleLivedMin65Years(people))
+// === [people[0], people[1]];
