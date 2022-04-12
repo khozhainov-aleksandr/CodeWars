af9928f7611c988fb9b4f4646289f03aa255e74f
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-12 09:14:39 +0300

    Get children

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,15 +1,19 @@
 'use strict';
 
-// Get people life durations
+// Get children
 
 const people = [
   { name: 'Carolus Haverbeke', sex: 'm', born: 1832, died: 1905, father: 'Carel Haverbeke', mother: 'Maria van Brussel' },
   { name: 'Emma de Milliano', sex: 'f', born: 1876, died: 1956, father: 'Petrus de Milliano', mother: 'Sophia van Damme' },
   { name: 'Maria de Rycke', sex: 'f', born: 1683, died: 1724, father: 'Frederik de Rycke', mother: 'Laurentia van Vlaenderen' },
+  { name: 'Carel Haverbeke', sex: 'm', born: 1796, died: 1837, father: 'Pieter Antone Haverbeke', mother: 'Livina Sierens' },
 ];
 
-function getPeopleLifeDurations(people) {
-  return people.map(el => (el.died - el.born));
+function getChildren(people, person) {
+  const parentName = person.name;
+  return people.filter(el => parentName === el.father || parentName === el.mother);
 }
 
-console.log("🚀 ~ file:", getPeopleLifeDurations(people)) // === [73, 80, 41];
+const person = people[3];
+
+console.log("🚀 ~ file:", getChildren(people, person)) // === [people[0]];
