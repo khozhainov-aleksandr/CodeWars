0b78bcbfa2948528e1ba9e62073d6d3a4102688b
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-12 09:00:52 +0300

    Get people names

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,17 +1,15 @@
 'use strict';
 
-const group = [
-  { name: 'A', marks: [2, 3, 5, 4] },
-  { name: 'B', marks: [2, 3, 5, 4] },
-  { name: 'C', marks: [5, 5, 5, 5] },
-  { name: 'D', marks: [2, 3, 5, 4] },
-  { name: 'E', marks: [2, 3, 5, 4] },
+// Get people names
+
+const people = [
+  { name: 'Carolus Haverbeke', sex: 'm', born: 1832, died: 1905, father: 'Carel Haverbeke', mother: 'Maria van Brussel' },
+  { name: 'Emma de Milliano', sex: 'f', born: 1876, died: 1956, father: 'Petrus de Milliano', mother: 'Sophia van Damme' },
+  { name: 'Maria de Rycke', sex: 'f', born: 1683, died: 1724, father: 'Frederik de Rycke', mother: 'Laurentia van Vlaenderen' },
 ];
 
-function hasExcellentStudent(studetns, goodMarks) {
-  return studetns
-    .some(element => (element.marks
-        .reduce((sum, el) => sum + el) / element.marks.length) === goodMarks);
+function getPeopleNames(people) {
+  return people.map(el => el.name);
 }
 
-console.log( hasExcellentStudent(group, 5) );
+console.log("🚀 ~ file:", getPeopleNames(people)) // === ['Carolus Haverbeke', Emma de Milliano', Maria de Rycke']
