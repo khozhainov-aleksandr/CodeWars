f218ef0ed023bf43eef90e586b5efe7fad548cd0
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-13 08:47:35 +0300

    Add to number

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,31 +1,13 @@
 'use strict';
 
-function createDictaphone(name) {
-  let accumulativeText = '';
+// Add to number
 
-  const dictaphone = (phrase = '') => {
-    accumulativeText += phrase;
-
-    return `${name} saved: ${accumulativeText}`;
-  }
-
-  dictaphone.reset = () => {
-    accumulativeText = '';
-  }
-
-  return dictaphone;
+function addToNumber(x) {
+  return (y) => {
+    return x + y;
+  };
 }
 
-const myDictaphone = createDictaphone('Aleksandr');
-
-myDictaphone('It ');
-myDictaphone('should ');
-myDictaphone('work!');
-
-myDictaphone.reset();
-myDictaphone('Hello ');
-myDictaphone('my ');
-myDictaphone('world!');
-
-const result = myDictaphone();
-console.log("🔥", result); // Aleksandr saved: Hello my world!
+console.log("🔥 => addToNumber(1)(2)", addToNumber(1)(2)); // === 3
+console.log("🔥 => addToNumber(0)(-4)", addToNumber(0)(-4)); // === -4
+console.log("🔥 => addToNumber(10)()", addToNumber(10)()); // === NaN
