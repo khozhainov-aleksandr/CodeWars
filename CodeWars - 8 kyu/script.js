825a5eb139c1dbbc6b54aa779cd3f793252b0516
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-13 08:58:01 +0300

    Multiply all elements

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,13 +1,14 @@
 'use strict';
 
-// Add to number
+// Multiply all elements
 
-function addToNumber(x) {
-  return (y) => {
-    return x + y;
+function multiplyAll(arr) {
+  return (num) => {
+    return arr.map(element => element * num);
   };
 }
 
-console.log("🔥 => addToNumber(1)(2)", addToNumber(1)(2)); // === 3
-console.log("🔥 => addToNumber(0)(-4)", addToNumber(0)(-4)); // === -4
-console.log("🔥 => addToNumber(10)()", addToNumber(10)()); // === NaN
+console.log("🔥 => multiplyAll([])(5)", multiplyAll([])(5)); // === []
+console.log("🔥 => multiplyAll([1, 2, 3])(0)", multiplyAll([1, 2, 3])(0)); // === [0, 0, 0]
+console.log("🔥 => multiplyAll([9, 10, 11])(2)", multiplyAll([9, 10, 11])(2)); // === [18, 20, 22]
+console.log("🔥 => multiplyAll([-3, 2, 1, -9, 4])(-1)", multiplyAll([-3, 2, 1, -9, 4])(-1)); // === [3, -2, -1, 9, -4]
