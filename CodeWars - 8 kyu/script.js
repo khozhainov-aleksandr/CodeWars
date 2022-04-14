e8262bb719e5b240229ce1bb6a5707f809c94043
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-14 09:45:14 +0300

    Pretty array

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,10 +1,26 @@
 'use strict';
 
-// Count occurrences
+// Pretty array
 
-function countOccurrences(phrase, part) {
-  return phrase.split(part).length - 1;
+function isArrayPretty(numbers) {
+  if (numbers.length === 0) {
+    return false;
+  }
+
+  for (let i = 0; i < numbers.length; i++) {
+    const big = numbers[i] + 1;
+    const small = numbers[i] - 1;
+
+    const res = numbers.some(el => el === big || el === small);
+
+    if (!res) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log("🔥 => countOccurrences('azyxxzyzy', 'zy')", countOccurrences('azyxxzyzy', 'zy')); // === 3
-console.log("🔥 => countOccurrences('ababagalamaga', 'bag')", countOccurrences('ababagalamaga', 'bag')); // === 1
+console.log( isArrayPretty([2, 10, 9, 3]) ); // true
+console.log( isArrayPretty([5, 6, 7]) ); // true
+console.log( isArrayPretty([6, 7, 1]) ); // false
