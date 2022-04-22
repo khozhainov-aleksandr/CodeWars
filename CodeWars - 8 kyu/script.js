76d6a72921ad1b8a92b5c73cb1c686d402da99bb
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-04-22 09:11:49 +0300

    Fix string

--- CodeWars - 8 kyu/script.js
+++ CodeWars - 8 kyu/script.js
@@ -1,24 +1,86 @@
 'use strict';
 
-// Omit method
-
-function omitMethod() {
-  /**
-   * @param {string[]} keys
-   *
-   * @returns {Object}
-   */
-  Object.prototype.omit = function omit(keys) {
-    const newObj = Object.assign({}, this);
-
-    for (const key in newObj) {
-      keys.forEach(element => {
-        if (key === element) {
-          delete newObj[key];
-        }
-      });
-    }
-
-    return newObj;
-  };
+/*
+User.prototype = {
+  seyHi() {
+    return `Hello! I am ${this.name}`;
+  },
+  sayBye() {
+    return `Bye! I am ${this.name}`;
+  }
 }
+
+function User(name) {
+  this.name = name;
+  this.role = 'user';
+  this.rating = 0;
+}
+
+const user1 = new User('Misha');
+const user2 = new User('Aleksandr');
+
+console.log( user1.seyHi() );
+console.log( user2.sayBye() );
+*/
+
+// Работает так же как и выше
+
+/*
+User.prototype.seyHi = function() {
+  return `Hello! I am ${this.name}`;
+}
+
+User.prototype.sayBye = function() {
+  return `Bye! I am ${this.name}`;
+}
+
+function User(name) {
+  this.name = name;
+  this.role = 'user';
+  this.rating = 0;
+}
+
+const user1 = new User('Misha');
+const user2 = new User('Aleksandr');
+
+console.log( user1.seyHi() );
+console.log( user2.sayBye() );
+*/
+
+// ------------- //
+
+// const father = {
+//   name: 'Mykola',
+//   lastName: 'Hrynko'
+// };
+
+// const misha = {
+//   name: 'Misha'
+// };
+
+// Object.setPrototypeOf(misha, father); 
+
+// console.log(misha.lastName); // 'Hrynko'
+
+// father.lastName = 'New last name';
+
+// console.log(misha.lastName); // 'New last name'
+
+// ------------------------------------------------------------------------------------------------------- //
+
+// Fix string
+
+function fixString(str) {
+  let small = 0;
+  let big = 0;
+
+  str.split('').filter(el => el !== ' ').forEach(element => {
+    (element === element.toUpperCase()) ? big++ : small++;
+  });
+
+  return (big > small) ? str.toUpperCase() : str.toLowerCase();
+}
+
+console.log("🔥 => fixString('Mate Academy')", fixString('Mate Academy')); // === 'mate academy'
+console.log("🔥 => fixString('Mate ACADEmy')", fixString('Mate ACADEmy')); // === 'MATE ACADEMY'
+console.log("🔥 => fixString('maTE')", fixString('maTE')); // === 'mate'
