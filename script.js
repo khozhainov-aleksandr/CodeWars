8f16b66af602e7a801a1201e15f80e084339fdf9
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-05-27 09:29:15 +0300

    Find parity outlier

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
 'use strict';
 
-function generateRows(num) {
-  const result = [];
+/* 
+// Missing number
 
-  for (let i = 1; i <= num; i++) {
-    if (i === 1) {
-      result.push([1]);
-    } else if (i === 2) {
-      result.push([1, 1]);
-    } else {
-      const preArr = result[result.length - 1];
-      const arr = new Array(i);
+function findMissingNumber(nums) {
+  const numsSort = nums.sort();
+  const acc = [];
 
-      arr[0] = 1;
-
-      for (let j = 0; j < i - 2; j++) {
-        arr[j + 1] = preArr[j] + preArr[j + 1];
-      }
-      arr[i - 1] = 1;
-      result.push(arr);
-    }
+  for (let i = 0; i <= nums.length; i++) {
+    acc.push(i);
   }
 
-  return result;
+  const res = acc.filter(el => !numsSort.includes(el))[0];
+
+  return res;
 }
 
-console.log(generateRows(1)); // === [[1]]
-console.log(generateRows(3)); // === [[1], [1, 1], [1, 2, 1]]
-console.log(generateRows(6)); // === [[1], [1, 1], [1, 2, 1], [1, 3, 3, 1], [1, 4, 6, 4, 1], [1, 5, 10, 10, 5, 1]]
+console.log( findMissingNumber([3,2,5,0,6,4,1]) ); // 7
+console.log( findMissingNumber([1,3,8,2,7,0,9,4,6]) ); // 5
+console.log( findMissingNumber([4,7,2,3,1,0,5]) ); // 6
+*/
+
+
+
+
+// Find parity outlier
+
+function findOutlier(nums) {
+  const even = nums.filter(el => el % 2 === 0)
+  const odd = nums.filter(el => el % 2 !== 0)
+
+  if (even.length > odd.length) {
+    return odd[0];
+  } else if (even.length < odd.length) {
+    return even[0];
+  }
+}
+
+console.log( findOutlier([0, 1, 2]) ); // === 1
+console.log( findOutlier([1, 2, 3]) ); // === 2
+console.log( findOutlier([2, 6, -8, 10, -3]) ); // === -3
+console.log( findOutlier([1, 1, 0, 1, 1]) ); // === 0
+console.log( findOutlier([-993,-5,-84,-33,-12345]) ); // === -84
+
