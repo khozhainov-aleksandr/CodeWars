dffb7c2caa4c6d50b80a450885ac53f937da6821
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-05-27 09:36:06 +0300

    Missing number

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
 
-/* 
+
 // Missing number
 
 function findMissingNumber(nums) {
@@ -19,27 +19,3 @@ function findMissingNumber(nums) {
 console.log( findMissingNumber([3,2,5,0,6,4,1]) ); // 7
 console.log( findMissingNumber([1,3,8,2,7,0,9,4,6]) ); // 5
 console.log( findMissingNumber([4,7,2,3,1,0,5]) ); // 6
-*/
-
-
-
-
-// Find parity outlier
-
-function findOutlier(nums) {
-  const even = nums.filter(el => el % 2 === 0)
-  const odd = nums.filter(el => el % 2 !== 0)
-
-  if (even.length > odd.length) {
-    return odd[0];
-  } else if (even.length < odd.length) {
-    return even[0];
-  }
-}
-
-console.log( findOutlier([0, 1, 2]) ); // === 1
-console.log( findOutlier([1, 2, 3]) ); // === 2
-console.log( findOutlier([2, 6, -8, 10, -3]) ); // === -3
-console.log( findOutlier([1, 1, 0, 1, 1]) ); // === 0
-console.log( findOutlier([-993,-5,-84,-33,-12345]) ); // === -84
-
