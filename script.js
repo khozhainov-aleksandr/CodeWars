3c9299f5166d71854e027bc418837b2fbfb48197
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-08-30 10:34:17 +0300

    Finish task

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 // Remove All The Marked Elements of a List
 // https://www.codewars.com/kata/563089b9b7be03472d00002b/train/javascript
 
@@ -29,6 +30,8 @@ console.log('===>', arr.remove_([8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 
 >>>>>>> b76f16b (Finish task)
 =======
 >>>>>>> f3d8ff4 (Finish task)
+=======
+>>>>>>> 75d174b (Finish task)
 // Basic Training: Add item to an Array
 
 const websites = [];
@@ -48,6 +51,8 @@ console.log('===>', websites);
 =======
 =======
 >>>>>>> d37c9fa (Finish task)
+=======
+>>>>>>> 490624d (Finish task)
 // Remove All The Marked Elements of a List
 // https://www.codewars.com/kata/563089b9b7be03472d00002b/train/javascript
 
@@ -76,6 +81,8 @@ console.log('===>', websites);
 =======
 =======
 =======
+=======
+>>>>>>> ac8d79d (Finish task)
 // Basic Training: Add item to an Array
 
 const websites = [];
@@ -84,6 +91,32 @@ websites.push('codewars')
 
 // eslint-disable-next-line no-console
 console.log('===>', websites);
+<<<<<<< HEAD
 >>>>>>> 9f62671 (Finish task)
+<<<<<<< HEAD
 >>>>>>> d37c9fa (Finish task)
+<<<<<<< HEAD
 >>>>>>> f3d8ff4 (Finish task)
+=======
+=======
+=======
+=======
+// Remove All The Marked Elements of a List
+// https://www.codewars.com/kata/563089b9b7be03472d00002b/train/javascript
+
+const arr = new Array();
+
+Array.prototype.remove_ = function(integer_list, values_list){
+  return integer_list.filter(el => !values_list.includes(el));
+}
+
+// eslint-disable-next-line no-console
+console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4], [1, 3])); // [2, 2, 4]
+// eslint-disable-next-line no-console
+console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2])); // [5, 6, 7, 8]
+// eslint-disable-next-line no-console
+console.log('===>', arr.remove_([8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3])); // [8, 7, 6, 5, 1]
+>>>>>>> 9a1fba8 (Finish task)
+>>>>>>> ac8d79d (Finish task)
+>>>>>>> 490624d (Finish task)
+>>>>>>> 75d174b (Finish task)
