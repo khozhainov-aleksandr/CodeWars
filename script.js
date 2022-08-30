490624d59d5b6f769ccfde6b0d942e87f9bf7a44
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-08-30 10:28:38 +0300

    Finish task

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 // Remove All The Marked Elements of a List
 // https://www.codewars.com/kata/563089b9b7be03472d00002b/train/javascript
 
@@ -17,6 +18,8 @@ console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [
 // eslint-disable-next-line no-console
 console.log('===>', arr.remove_([8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3])); // [8, 7, 6, 5, 1]
 =======
+=======
+>>>>>>> ac8d79d (Finish task)
 // Basic Training: Add item to an Array
 
 const websites = [];
@@ -25,4 +28,24 @@ websites.push('codewars')
 
 // eslint-disable-next-line no-console
 console.log('===>', websites);
+<<<<<<< HEAD
 >>>>>>> 9f62671 (Finish task)
+=======
+=======
+// Remove All The Marked Elements of a List
+// https://www.codewars.com/kata/563089b9b7be03472d00002b/train/javascript
+
+const arr = new Array();
+
+Array.prototype.remove_ = function(integer_list, values_list){
+  return integer_list.filter(el => !values_list.includes(el));
+}
+
+// eslint-disable-next-line no-console
+console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4], [1, 3])); // [2, 2, 4]
+// eslint-disable-next-line no-console
+console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2])); // [5, 6, 7, 8]
+// eslint-disable-next-line no-console
+console.log('===>', arr.remove_([8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3])); // [8, 7, 6, 5, 1]
+>>>>>>> 9a1fba8 (Finish task)
+>>>>>>> ac8d79d (Finish task)
