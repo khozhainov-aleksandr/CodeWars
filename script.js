d37c9fa8c8225eccd749eb45a4deaabc4cc16303
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-08-30 10:28:33 +0300

    Finish task

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+<<<<<<< HEAD
 // Remove All The Marked Elements of a List
 // https://www.codewars.com/kata/563089b9b7be03472d00002b/train/javascript
 
@@ -15,3 +16,13 @@ console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4], [1, 3])); // [2, 2, 4]
 console.log('===>', arr.remove_([1, 1, 2, 3, 1, 2, 3, 4, 4, 3, 5, 6, 7, 2, 8], [1, 3, 4, 2])); // [5, 6, 7, 8]
 // eslint-disable-next-line no-console
 console.log('===>', arr.remove_([8, 2, 7, 2, 3, 4, 6, 5, 4, 4, 1, 2, 3], [2, 4, 3])); // [8, 7, 6, 5, 1]
+=======
+// Basic Training: Add item to an Array
+
+const websites = [];
+
+websites.push('codewars')
+
+// eslint-disable-next-line no-console
+console.log('===>', websites);
+>>>>>>> 9f62671 (Finish task)
