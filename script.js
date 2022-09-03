d25a12b22b78bd4bfa20efadb0659d0f43d0b734
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-03 15:26:04 +0300

    Done

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+// Grasshopper - Debug
+
+function weatherInfo (temp) {
+  const c = convertToCelsius(temp);
+
+  if (c < 0)
+    return (c + " is freezing temperature")
+  else
+    return (c + " is above freezing temperature")
+}
+
+function convertToCelsius (temperature) {
+  return (temperature - 32) * (5 / 9);
+}
+
+console.log('===>', weatherInfo(50)); // '10 is above freezing temperature'
+
+console.log('===>', weatherInfo(23)); // '-5 is freezing temperature'
