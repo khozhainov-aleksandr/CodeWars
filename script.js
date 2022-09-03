af25d19d963a06d788f7c55486dbc950b5eb6f98
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-03 14:52:33 +0300

    Done

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 'use strict';
 
+function typeValidation(variable, type) {
+  return typeof variable === type;
+}
+
+// eslint-disable-next-line no-console
+console.log('===>', typeValidation(42, "number")); // true
+
+// eslint-disable-next-line no-console
+console.log('===>', typeValidation("42", "number")); // false
