aad717697dc3b0da7a41da4333b2b2e962dd3bf4
Aleksandr Khozhainov <khozhainov.aleksandr@gmail.com>
2022-09-03 15:08:17 +0300

    prepare task

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 'use strict';
 
+// Which section did you scroll to?
+
+function getSectionIdFromScroll(scrollY, sizes) {
+
+}
+
+console.log('===>', getSectionIdFromScroll(299,[300,200,400,600,100])); // 0
+
+console.log('===>', getSectionIdFromScroll(300, [300,200,400,600,100])); // 1
+
+console.log('===>', getSectionIdFromScroll(1600, [300,200,400,600,100])); // -1
