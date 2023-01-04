54fcf01d239148c9209821612da56a221f7367cc
Aleksandr <khozhainov.aleksandr@gmail.com>
2023-01-04 11:33:32 +0200

    Finish

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 'use strict';
 
+// How many lightsabers do you own?
+
+const howManyLightsabersDoYouOwn = (name) => name === 'Zach' ? 18 : 0;
+
+console.log('✅', howManyLightsabersDoYouOwn()); // 0
+console.log('✅', howManyLightsabersDoYouOwn("Adam")); // 0
+console.log('✅', howManyLightsabersDoYouOwn("Zach")); // 18
